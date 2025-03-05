f69f491cfe9e897dd3483add308d116df6466de6
Achmad Sobari <ahmadsobarry@gmail.com>
2025-03-05 07:31:36 +0700

    Edit form input penugasan.

--- app/Filament/Resources/PenugasanResource.php
+++ app/Filament/Resources/PenugasanResource.php
@@ -7,16 +7,18 @@ use Filament\Tables;
 use Filament\Forms\Form;
 use App\Models\Penugasan;
 use Filament\Tables\Table;
-use Filament\Forms\Components\TextInput;
 use Filament\Resources\Resource;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Textarea;
+use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\DatePicker;
 use Illuminate\Database\Eloquent\Builder;
 use Filament\Forms\Components\Section as Section;
 use App\Filament\Resources\PenugasanResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\PenugasanResource\RelationManagers\PegawaiRelationManager;
-use Filament\Tables\Columns\IconColumn;
 
 class PenugasanResource extends Resource
 {
@@ -30,28 +32,44 @@ class PenugasanResource extends Resource
     {
         return $form
             ->schema([
-                TextInput::make('nama_st')
-                    ->required(),
-                Forms\Components\DatePicker::make('tanggal_st')
-                    ->required(),
-                TextInput::make('dasar_st')
-                    ->required(),
-                TextInput::make('perihal_dasar')
-                    ->required(),
-                DatePicker::make('tanggal_mulai')
-                    ->required(),
-                DatePicker::make('tanggal_selesai')
-                    ->required(),
-                TextInput::make('agenda')
-                    ->required(),
-                TextInput::make('jenis_st')
-                    ->required(),
-                TextInput::make('tujuan')
-                    ->required(),
+                Group::make()
+                ->schema([
+                    Section::make('Detail Perjalanan Dinas')
+                        ->schema([
+                            TextInput::make('nama_st')
+                                ->required(),
+                            DatePicker::make('tanggal_st')
+                                ->label('Tanggal ST')
+                                ->native(false)
+                                ->displayFormat('d mm Y')
+                                ->required(),
+                            TextInput::make('dasar_st')
+                                ->required(),
+                            TextInput::make('perihal_dasar')
+                                ->required(),
+                            Textarea::make('agenda')
+                                ->required(),
+                            TextInput::make('jenis_st')
+                                ->required(),
+                            TextInput::make('tujuan')
+                                ->required(),
+                    ])->columns(2)
+                ])->columnSpan(['lg' => 2]),
+                Group::make()
+                    ->schema([
+                        Section::make('Waktu')
+                            // ->description('Prevent abuse by limiting the number of requests per period')
+                            ->schema([
+                                DatePicker::make('tanggal_mulai')
+                                    ->required(),
+                                DatePicker::make('tanggal_selesai')
+                                    ->required(),
+                            ])
+                ])->columnSpan(['lg' => 1]),
                 // Forms\Components\Section::make('Pegawai yang Ditugaskan')->schema([
                 //     Forms\Components\Select::make('pegawai')->multiple()->relationship('pegawai', 'nama'),
                 // ]),
-            ]);
+            ])->columns(3);
     }
 
     public static function table(Table $table): Table
@@ -66,7 +84,7 @@ class PenugasanResource extends Resource
                 TextColumn::make('tanggal_selesai')
                     ->date()
                     ->sortable(),
-                TextColumn::make('tujuan')->badge(),
+                TextColumn::make('tujuan')->badge()->color('danger'),
                 TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
